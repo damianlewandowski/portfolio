1bb0f585587baef65abc2c7c13b4f0ee450da8ae
damian <damian20lewandowski@gmail.com>
2018-02-21 11:21:51 +0100

    minor changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import home from "../../images/home.jpg"
+
 import './Home.css';
 
+const style = {
+  background: `url(${home}) no-repeat center center fixed`
+}
+
 const Home = () => (
-  <div id="home" className="Home">
+  <div id="home" className="Home" style={style}>
     <div className="info">
       <h4>DAMIAN LEWANDOWSKI</h4>
       <h1>ASPIRING WEB DEVELOPER</h1>
